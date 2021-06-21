5d8d67163466389a3164507206f6bf95083df2c9
Leo <leo.vander.meulen@gmail.com>
2021-06-21 21:09:15 +0200

    Initial version

---
+++ DataUpdater.py
@@ -0,0 +1,93 @@
+import requests
+import json
+import urllib
+import zipfile
+import re
+import cbsodata
+import osmnx as ox
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import xml.etree.ElementTree as ElementTree
+from bs4 import BeautifulSoup
+
+# TODO: Utrecht en Groningen downloaden alleen de stad, dit moet de provincie worden
+for name in ['Gelderland', 'Overijssel', 'Drenthe', 'Groningen', 'Friesland', 'Zeeland', 'Utrecht',
+             'Limburg', 'Noord-Holland', 'Zuid-Holland', 'Flevoland', 'Noord-Brabant']:
+    print(name)
+    G = ox.graph_from_place(name + ", Netherlands", network_type="drive")
+    gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+    names = gdf_edges[['name']].reset_index().drop(columns={'u', 'v', 'key'})
+    names.name = names.name.astype('str')
+    names = names.drop_duplicates()
+    names.name = names.name.astype('str')
+    names = names[~names.name.str.contains('\[')]
+    print(len(names))
+    names.to_csv('datasets\\RAW_streets\\streets_' + name + '.csv', index=False)
+
+lst = []
+for name in ['Gelderland', 'Overijssel', 'Drenthe', 'Groningen', 'Friesland', 'Zeeland', 'Utrecht',
+             'Limburg', 'Noord-Holland', 'Zuid-Holland', 'Flevoland', 'Noord-Brabant']:
+    lst.append(pd.read_csv('datasets\\RAW_streets\\streets_' + name + '.csv'))
+self.straatnamen = pd.concat(lst, axis=0, ignore_index=True).drop_duplicates()
+self.straatnamen.to_csv('datasets\\streets_Nederland.csv', index=False)
+
+total = pd.DataFrame(cbsodata.get_data('84992NED'))
+plaatsen = np.append(
+    np.append(np.append(total.Woonplaatsen.values, total.Naam_2.values), total.Naam_4.values),
+    total.Naam_6.values)
+self.plaatsen = pd.DataFrame(plaatsen, columns=['plaatsnaam']).drop_duplicates()
+self.plaatsen.to_csv("datasets\\places.csv", index=False)
+
+url = "http://www.naamkunde.net/?page_id=293&vt_download_xml=true"
+urllib.request.urlretrieve(url, "datasets\\RAW_names\\firstnames.xml")
+xtree = ElementTree.parse("datasets\\RAW_names\\firstnames.xml")
+xroot = xtree.getroot()
+voornamen = []
+for node in xroot:
+    voornaam = node.find("voornaam")
+    if voornaam is not None:
+        voornamen.append({"voornaam": voornaam.text})
+self.voornamen = pd.DataFrame(voornamen)
+self.voornamen.to_csv("datasets\\firstnames.csv", index=False)
+
+url = "http://www.naamkunde.net/wp-content/uploads/oudedocumenten/fn10k_versie1.zip"
+urllib.request.urlretrieve(url, "datasets\\RAW_names\\lastnames.zip")
+zf = zipfile.ZipFile("datasets\\RAW_names\\lastnames.zip", 'r')
+f = zf.open("fn_10kw.xml")
+xtree = ElementTree.parse(f)
+xroot = xtree.getroot()
+achternamen = []
+for node in xroot:
+    achternaam = node.find("naam")
+    voorvoegsel = node.find("prefix")
+    if achternaam is not None:
+        if voorvoegsel.text is not None:
+            achternamen.append({"achternaam": voorvoegsel.text + " " + achternaam.text})
+        else:
+            achternamen.append({"achternaam": achternaam.text})
+self.achternamen = pd.DataFrame(achternamen)
+self.achternamen.to_csv("datasets\\lastnames.csv", index=False)
+
+url = 'https://nl.wikibooks.org/wiki/Geneeskunde/Lijst_van_aandoeningen'
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'lxml')
+div = soup.find(id="mw-content-text")
+aandoeningen = []
+for tag in div.find_all("li"):
+    aandoeningen.append(tag.text)
+self.aandoeningen = pd.DataFrame(aandoeningen, columns=['aandoening'])
+self.aandoeningen.to_csv("datasets\\aandoeningen.csv", index=False)
+
+# TODO Automatisch parsen van de XLSX zonder handmatige conversie naar CVS
+url = "https://www.ema.europa.eu/sites/default/files/Medicines_output_referrals.xlsx"
+urllib.request.urlretrieve(url, "datasets\\RAW_medicijnen\\medicines.xlsx")
+# !!! Convert to csv in Excel
+medicijnen = pd.read_csv("datasets\\RAW_medicijnen\\medicines.csv", sep=';')
+lijst = []
+for m in medicijnen.iloc[:, 2]:
+    if len(str(m)) > 3:
+        for n in m.split(','):
+            lijst.append(re.sub(r"\([^()]*\)", "", n).strip())
+self.medicijnen = pd.DataFrame(lijst, columns=['medicijn']).drop_duplicates()
+self.medicijnen.to_csv("datasets\\medicijnen.csv", index=False)
---
+++ PrivacyFilter.py
@@ -0,0 +1,118 @@
+import time
+import re
+from flashtext import KeywordProcessor
+
+
+class PrivacyFilter:
+
+    def __init__(self):
+        self.keyword_processor_case_sensitive = KeywordProcessor(case_sensitive=True)
+        self.keyword_processor_case_insensitive = KeywordProcessor(case_sensitive=False)
+        self.initialised = False
+
+    def file_to_list(self, filename, minimum_length=0, drop_first=1):
+        with open(filename, encoding='latin') as f:
+            lst = [line.rstrip() for line in f]
+        lst = list(dict.fromkeys(lst))
+        if minimum_length > 0:
+            lst = list(filter(lambda item: len(item) > minimum_length, lst))
+        return lst[drop_first:]
+
+    def initialize(self):
+
+        for naam in self.file_to_list('datasets\\streets_Nederland.csv', minimum_length=5):
+            for c in ['.', ',', ' ', ':', ';', '?', '!']:
+                self.keyword_processor_case_insensitive.add_keyword(naam + c, '<ADRES>' + c)
+
+        for naam in self.file_to_list('datasets\\places.csv'):
+            for c in ['.', ',', ' ', ':', ';', '?', '!']:
+                self.keyword_processor_case_insensitive.add_keyword(naam + c, '<PLAATS>' + c)
+
+        for naam in self.file_to_list('datasets\\firstnames.csv'):
+            self.keyword_processor_case_sensitive.add_keyword(naam, '<NAAM>')
+
+        for naam in self.file_to_list('datasets\\lastnames.csv'):
+            self.keyword_processor_case_sensitive.add_keyword(naam, '<NAAM>')
+
+        for naam in self.file_to_list('datasets\\aandoeningen.csv'):
+            self.keyword_processor_case_insensitive.add_keyword(naam, '<AANDOENING>')
+
+        for naam in self.file_to_list('datasets\\medicijnen.csv'):
+            self.keyword_processor_case_insensitive.add_keyword(naam, '<MEDICIJN>')
+
+        self.initialised = True
+
+    def remove_numbers(self, text, set_zero=True):
+        if set_zero:
+            return re.sub('\d', '0', text).strip()
+        else:
+            return re.sub(r'\w*\d\w*', '<GETAL>', text).strip()
+
+    def remove_dates(self, text):
+        text = re.sub("\d{2}[- /.]\d{2}[- /.]\d{,4}", "<DATUM> ", text)
+
+        text = re.sub(
+            "(\d{1,2}[^\w]{,2}(januari|februari|maart|april|mei|juni|juli|augustus|september|oktober|november|december)([- /.]{,2}(\d{4}|\d{2})){,1})(?P<n>\D)(?![^<]*>)",
+            "<DATUM> ",
+            text)
+
+        text = re.sub(
+            "(\d{1,2}[^\w]{,2}(jan|feb|mrt|apr|mei|jun|jul|aug|sep|okt|nov|dec)([- /.]{,2}(\d{4}|\d{2})){,1})(?P<n>\D)(?![^<]*>)",
+            "<DATUM> ",
+            text)
+        return text
+
+    def remove_email(self, text):
+        return re.sub("(([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?))(?![^<]*>)",
+                      "<EMAIL>",
+                      text)
+
+    def remove_postal_codes(self, text):
+        return re.sub("[0-9]{4}[ ]?[A-Z]{2}([ ,.:;])", "<POSTCODE>\\1", text)
+
+    def filter(self, inputtext, set_numbers_zero=True):
+        if not self.initialised:
+            return inputtext
+
+        text = " " + inputtext + " "
+
+        text = self.remove_dates(text)
+        text = self.remove_email(text)
+        text = self.remove_postal_codes(text)
+        text = self.remove_numbers(text, set_numbers_zero)
+
+        text = self.keyword_processor_case_insensitive.replace_keywords(text)
+        text = self.keyword_processor_case_sensitive.replace_keywords(text)
+
+        return text.strip()
+
+
+def insert_newlines(string, every=64):
+    return '\n'.join(string[i:i + every] for i in range(0, len(string), every))
+
+
+def main():
+    zin = "De mogelijkheden zijn sinds 2014 groot geworden, zeker vergeleken met 2012, hè Kees? Het systeem maakt " \
+          "verschillende bewerkningen mogelijk die hiervoor niet mogelijk waren. De datum is 24–01–2011 (of 24 jan 21 "\
+          "of 24 januari 2011). Ik ben te bereiken op naam@hostingpartner.nl en woon in Arnhem. Mijn adres is " \
+          "Maasstraat 231, 1234AB. Mijn naam is Thomas Janssen en ik heb zweetvoeten. Oh ja, ik gebruik hier " \
+          "ranitidine voor. "
+
+    print(insert_newlines(zin, 120))
+    start = time.time()
+
+    pfilter = PrivacyFilter()
+    pfilter.initialize()
+
+    print('\nConstructor time %4.0f msec' % ((time.time() - start) * 1000))
+
+    start = time.time()
+    nr_sentences = 1000
+    for i in range(0, nr_sentences):
+        zin = pfilter.filter(zin, set_numbers_zero=False)
+    print('Deduce time per sentence %4.2f msec\n' % ((time.time() - start) * 1000 / nr_sentences))
+    print(insert_newlines(zin, 120))
+
+
+if __name__ == "__main__":
+    main()
