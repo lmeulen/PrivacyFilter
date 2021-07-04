7e0fd864949335597f351475b28542ae95c97938
Leo <leo.vander.meulen@gmail.com>
2021-07-04 08:53:14 +0200

    Remove main function in API

--- PrivacyFilterAPI.py
+++ PrivacyFilterAPI.py
@@ -29,8 +29,8 @@ async def filtertext(item: Parameter):
     else:
         return {"message": "No text specified"}
 
-if __name__ == '__main__':
-    uvicorn.run("PrivacyFilterAPI:privacyFilterApp",
-                host="0.0.0.0",
-                port=8000,
-                reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("PrivacyFilterAPI:privacyFilterApp",
+#                 host="0.0.0.0",
+#                 port=8000,
+#                 reload=True)
