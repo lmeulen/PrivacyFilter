13074c3fa6c322e8faf9ebb004f1ebd6a4997d02
Leo <leo.vander.meulen@gmail.com>
2021-07-04 12:05:31 +0200

    Remove inputtext from return

--- PrivacyFilterAPI.py
+++ PrivacyFilterAPI.py
@@ -23,11 +23,10 @@ async def root():
 @privacyFilterApp.post("/filter")
 async def filtertext(item: Parameter):
     starttime = time.time()
-    item_dict = item.dict()
     filtered_text = pfilter.filter(item.text, nlp_filter=item.use_nlp)
-    item_dict.update({"filtered": filtered_text})
-    item_dict.update({"time": time.time() - starttime})
-    return item_dict
+    return {"text": filtered_text,
+            "time": time.time() - starttime}
+
 
 if __name__ == '__main__':
     uvicorn.run("PrivacyFilterAPI:privacyFilterApp",
