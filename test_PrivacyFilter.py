d37bfdff14b8deb6088887a2c4f6d4cfaee795fd
Leo <leo.vander.meulen@gmail.com>
2021-07-07 07:49:25 +0200

    Correct expected-actual order

--- test_PrivacyFilter.py
+++ test_PrivacyFilter.py
@@ -20,27 +20,27 @@ class TestRegex(PFTest):
     def test_url(self):
         for sample in file_to_samples("url.txt"):
             dirty, clean = sample
-            self.assertEqual(self.pfilter.remove_url(dirty), clean)
+            self.assertEqual(clean, self.pfilter.remove_url(dirty))
 
     def test_email(self):
         for sample in file_to_samples("email.txt"):
             dirty, clean = sample
-            self.assertEqual(self.pfilter.remove_email(dirty), clean)
+            self.assertEqual(clean, self.pfilter.remove_email(dirty))
 
     def test_date(self):
         for sample in file_to_samples("date.txt"):
             dirty, clean = sample
-            self.assertEqual(self.pfilter.remove_dates(dirty), clean)
+            self.assertEqual(clean, self.pfilter.remove_dates(dirty))
 
     def test_postal_codes(self):
         for sample in file_to_samples("postal_codes.txt"):
             dirty, clean = sample
-            self.assertEqual(self.pfilter.remove_postal_codes(dirty), clean)
+            self.assertEqual(clean, self.pfilter.remove_postal_codes(dirty))
 
     def test_numbers(self):
         for sample in file_to_samples("numbers.txt"):
             dirty, clean = sample
-            self.assertEqual(self.pfilter.remove_numbers(dirty), clean)
+            self.assertEqual(clean, self.pfilter.remove_numbers(dirty))
 
 if __name__ == '__main__':
     unittest.main()
